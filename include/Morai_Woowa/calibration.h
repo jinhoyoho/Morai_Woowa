9cc2f802ef03d2b5e9ece918bda9e038ec57057b
jinhoyoho <jinho7018@naver.com>
2024-09-29 01:50:49 +0900

    Fix: camera 내부 파라미터 값 추가

--- include/Morai_Woowa/calibration.h
+++ include/Morai_Woowa/calibration.h
@@ -12,15 +12,36 @@
 
 using Eigen::MatrixXd;
 
+ // focal length
+double fx = 320.0;
+double fy = 320.0;
+
+// principal points
+double cx = 320.0;
+double cy = 240.0;
+
+// skew coefficient
+double skew_c = 0;
+
+
 class calibration
 {
 private:
     sensor_msgs::PointCloud2 lidar;
     ros::Subscriber image_sub;
+    ros::Subscriber lidar_sub;
     ros::Publisher pub;
-    cv::Mat frame;
+    cv::Mat frame;  // 이미지
+    Eigen::MatrixXd lidar_points;   // 라이다
+    Eigen::Matrix3d intrinsic;  // 카메라 내부 파라미터 3x3
+    Eigen::Matrix3d extrinsic;  // 카메라 외부 파라미터 3x3
+
+
 
 public:
-    calibration();
-    void image_callBack(const sensor_msgs::ImageConstPtr& msg);
+    calibration();  // 생성자
+    
+    void do_cali();  // calibration 실행
+    void image_callBack(const sensor_msgs::ImageConstPtr& msg); // 이미지 받기
+    void lidar_callBack(const sensor_msgs::PointCloud2ConstPtr& msg); // Lidar 받기
 };
--- src/sensor/calibration.cpp
+++ src/sensor/calibration.cpp
@@ -5,7 +5,13 @@ calibration::calibration()
 {
     ros::NodeHandle nh;
     image_sub = nh.subscribe("python_image", 1, &calibration::image_callBack, this);
-    pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+    lidar_sub = nh.subscribe("lidar_pre", 1, &calibration::lidar_callBack, this);
+
+    // camera 내부 파라미터 값 저장
+    intrinsic << fx, skew_c, cx,
+                 0, fy, cy,
+                 0, 0, 1;
+
 }
 
 void calibration::image_callBack(const sensor_msgs::ImageConstPtr& msg)
@@ -26,15 +32,45 @@ void calibration::image_callBack(const sensor_msgs::ImageConstPtr& msg)
     }
 }
 
+void calibration::lidar_callBack(const sensor_msgs::PointCloud2ConstPtr& msg)
+{
+    pcl::PCLPointCloud2* cloud_intermediate = new pcl::PCLPointCloud2;
+    pcl::PointCloud<pcl::PointXYZ> cloud;   // pointcloud 저장
+
+    // 포인터가 가리키는 포인트 클라우드 데이터를 cloud_intermediate에 저장
+    pcl_conversions::toPCL(*msg, *cloud_intermediate);
+    // cloud_intermediate에 저장된 PCL 포인트 클라우드 데이터를 cloud 객체로 변환
+    pcl::fromPCLPointCloud2(*cloud_intermediate, cloud);
+
+    Eigen::MatrixXd points(cloud.size(), 3); // x, y, z
+
+    for (size_t i = 0; i < cloud.size(); ++i) {
+        points(i, 0) = cloud.points[i].x;       // x
+        points(i, 1) = cloud.points[i].y;       // y
+        points(i, 2) = cloud.points[i].z;       // z
+
+    }
+
+    lidar_points = points;  // 변수에 저장
+}
+
+void calibration::do_cali()
+{
+
+
+}
+
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "calibration");
 
-    calibration cl;
+    ROS_INFO("Start Calibration...");
 
+    calibration cl;
     LiDAR_pre lp;
 
     ros::spin();
+
     return 0;
 }
