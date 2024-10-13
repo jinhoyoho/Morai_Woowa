8526e14d8235cf53c818ad34fde113889c6877cd
jimin3722 <jimin372215@gmail.com>
2024-10-13 18:38:36 +0900

    Add : tracking stop service

--- src/state_node.cpp
+++ src/state_node.cpp
@@ -8,6 +8,7 @@
 #include <actionlib/client/simple_action_client.h>
 #include "morai_woowa/Planning_Tracking_ActAction.h"
 #include "morai_woowa/Person_Collision_ActAction.h"
+#include "morai_woowa/StopTrackingSrv.h"
 
 #include <vector>
 #include <fstream>
@@ -26,47 +27,66 @@ struct Waypoint {
     int index;
 };
 
-typedef std::vector<Waypoint> path;
+struct Spot {
+    float x;
+    float y;
+    Spot(float x_coord, float y_coord) : x(x_coord), y(y_coord) {}
+};
+
+using path = std::vector<Waypoint>;
 
 class StateNode{ 
+
 public:
     StateNode(): 
     planning_tracking_ac_(nh_, "planning_tracking_action", true),
     person_collision_ac_(nh_, "person_collision_action", true)
     {
+        current_pose_sub_ = nh_.subscribe("/current_pose", 10, &StateNode::currentPoseCallback, this);
+        waypoint_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/waypoints", 10);  // PoseStamped 퍼블리셔
+        waypoint_marker_pub_ = nh_.advertise<visualization_msgs::Marker>("/globalpath", 10, this);
+
+        delivery_pickup_client_ = nh_.serviceClient<morai_msgs::WoowaDillyEventCmdSrv>("/WoowaDillyEventCmd");
+        stop_tracking_client_ = nh_.serviceClient<morai_woowa::StopTrackingSrv>("/StopTracking");
+
         if (planning_tracking_ac_.waitForServer(ros::Duration(5.0))) {  // 5초 대기
             ROS_INFO("Connected to planning_tracking server");
         } else {
             ROS_WARN("planning_tracking server not available!");
         }
         
-        if (person_collision_ac_.waitForServer(ros::Duration(5.0))) {  // 5초 대기
+        if (person_collision_ac_.waitForServer(ros::Duration(5.0))) {  
             ROS_INFO("Connected to person_collision server");
         } else {
             ROS_WARN("person_collision server not available!");
         }
 
-        closest_index_ = -1;
-
         is_planning_action_finished_ = false;
-        is_reach_target_point = false;
+        is_reach_target_point_ = false;
+        progress_percentage_ = 0;
 
+        is_found_collision_target_ = false;
         is_collision_action_finished_ = false;
-        is_success_collision = false;
+        is_success_collision_ = false;
 
-        delivery_pickup_client_ = nh_.serviceClient<morai_msgs::WoowaDillyEventCmdSrv>("/WoowaDillyEventCmd");
+        wpt_init_flag_ = false;
 
-        current_pose_sub_ = nh_.subscribe("/current_pose", 10, &StateNode::currentPoseCallback, this);
-        waypoint_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/waypoints", 10);  // PoseStamped 퍼블리셔
-        waypoint_marker_pub_ = nh_.advertise<visualization_msgs::Marker>("/globalpath", 10, this);
+        closest_index_ = -1;
+        cross_track_error = 10000000; 
+        current_x_ = 0;
+        current_y_ = 0;
+
     }
 
     void currentPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+        
+        current_x_ = msg->pose.position.x;
+        current_y_ = msg->pose.position.y;
 
-        closest_index_ = findClosestWaypoint(msg->pose.position.x, msg->pose.position.y, closest_index_, waypoints_);
+        closest_index_ = findClosestWaypoint(current_x_, current_y_, closest_index_, waypoints_);
         
         if (closest_index_ != -1) {
-            ROS_INFO("Current Position: (X: %.2f, Y: %.2f) is nearest to Waypoint Index: %d", msg->pose.position.x, msg->pose.position.y, closest_index_);
+            ROS_INFO("Current Position: (X: %.2f, Y: %.2f) is nearest to Waypoint Index: %d", current_x_, current_x_, closest_index_);
             ROS_INFO("cross_track_error: %lf", cross_track_error);
         } else {
             ROS_WARN("No waypoints available.");
@@ -95,33 +115,6 @@ public:
             // PoseStamped 퍼블리시
             waypoint_pub_.publish(pose_stamped);
         }
-    }
-    void publishMarker(path waypoints) {
-
-        visualization_msgs::Marker marker;
-        marker.header.frame_id = "map"; // RViz에서 사용할 프레임
-        marker.header.stamp = ros::Time::now();
-        marker.ns = "waypoints";
-        marker.action = visualization_msgs::Marker::ADD;
-        marker.type = visualization_msgs::Marker::SPHERE_LIST;
-        marker.scale.x = 0.1;
-        marker.scale.y = 0.1;
-        marker.scale.z = 0.1;
-        marker.color.r = 1.0;
-        marker.color.g = 0.0;
-        marker.color.b = 0.0;
-        marker.color.a = 1.0;
-
-        for (const auto& waypoint : waypoints) {
-            geometry_msgs::Point p;
-            p.x = waypoint.x;
-            p.y = waypoint.y;
-            p.z = 0; // Z축은 0으로 설정
-
-            marker.points.push_back(p);
-        }
-
-        waypoint_marker_pub_.publish(marker);
     }
 
     bool delivery(int item_index){
@@ -157,13 +150,12 @@ public:
         return pick_srv.response.response.result;
     }
 
+    void loadWaypoints(std::string waypoint_file, path &waypoints) {
 
-    void loadWaypoints(std::string waypoint_file_, path &waypoints_) {
-
-        wpt_init_flag = false;
+        wpt_init_flag_ = false;
         int cnt = 0;
 
-        std::ifstream file(waypoint_file_);
+        std::ifstream file(waypoint_file);
         if (file.is_open()) {
             Waypoint wp;
             std::string line;
@@ -194,16 +186,16 @@ public:
                 cnt ++;
             }
             file.close();
-            ROS_INFO("Loaded waypoints from %s", waypoint_file_.c_str());
+            ROS_INFO("Loaded waypoints from %s", waypoint_file.c_str());
         } else {
-            ROS_WARN("Unable to open waypoint file: %s", waypoint_file_.c_str());
+            ROS_WARN("Unable to open waypoint file: %s", waypoint_file.c_str());
         }
     }
 
-    int findClosestWaypoint(double x, double y, int previous_index, path waypoints) {
+    int findClosestWaypoint(float x, float y, int previous_index, path waypoints) {
 
         // 처음엔 모든 wpt를 돌면서 위치 체크
-        if(!wpt_init_flag){
+        if(!wpt_init_flag_){
             int closest_index = -1;
             double min_distance = std::numeric_limits<double>::max();
 
@@ -216,8 +208,7 @@ public:
                     cross_track_error = min_distance;
                 }
             }
-
-            wpt_init_flag = true;
+            wpt_init_flag_ = true;
 
             return closest_index;
         }
@@ -253,11 +244,11 @@ public:
         is_planning_action_finished_ = true;
 
         if (result->success){
-            is_reach_target_point = true;
+            is_reach_target_point_ = true;
             std::cout << "state node : planning action finished successfully" << std::endl;
         } 
         else {
-            is_reach_target_point = false;
+            is_reach_target_point_ = false;
             std::cout << "state node : Action did not finish successfully" << std::endl;
         }
     }
@@ -268,6 +259,7 @@ public:
 
     void PlanningActionFeedbackCb(const morai_woowa::Planning_Tracking_ActFeedbackConstPtr& feedback){
         std::cout << "state node : planner action feedback: " << feedback->progress_percentage << std::endl;
+        progress_percentage_ = feedback->progress_percentage;
     }
 
     void PersonCollisionActionDoneCb(const actionlib::SimpleClientGoalState& state,
@@ -276,11 +268,11 @@ public:
         is_collision_action_finished_ = true;
 
         if (result->success){
-            is_success_collision = true;
+            is_success_collision_ = true;
             std::cout << "state node : collision action finished successfully" << std::endl;
         } 
         else {
-            is_success_collision = false;
+            is_success_collision_ = false;
             std::cout << "state node : collision Action did not finish successfully" << std::endl;
         }    
     }
@@ -290,27 +282,52 @@ public:
     }
 
     void PersonCollisionActionFeedbackCb(const morai_woowa::Person_Collision_ActFeedbackConstPtr& feedback){
+
         std::cout << "state node : planner action feedback: " << feedback->is_target << std::endl;
+        if (feedback->is_target){
+            is_found_collision_target_ = true;
+        } 
+        else {
+            is_found_collision_target_ = false;
+        } 
     }
 
-    bool set_target_point(int a, int b, bool indoor){
+    int request_planning_point(int a, int b, bool indoor){
 
-        std::ostringstream oss;
+        std::ostringstream oss1;
+        std::ostringstream oss2;
         std::string filename;
-        
-        if (indoor) {
-            oss << "indoor_" << a << "_" << b << ".csv";
-            filename = oss.str();
-        }
-        else{
-            oss << "outdoor_" << a << "_" << b << ".csv";
-            filename = oss.str();
-        }
-
-        loadWaypoints(filename, waypoints_);
-
+        std::string out_or_in;
         morai_woowa::Planning_Tracking_ActGoal planning_goal;
-        planning_goal.path = filename;
+
+        if (indoor) 
+            out_or_in = "indoor";
+        else
+            out_or_in = "outdoor";
+
+        oss1 << out_or_in << "_" << a << "_" << b << ".csv";
+        filename = oss1.str();
+
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            planning_goal.path = filename;  
+            planning_goal.reverse = false;  
+        }
+        else {
+            oss2 << out_or_in << "_" << b << "_" << a << ".csv";
+            filename = oss2.str();
+            std::ifstream file(filename);
+
+            if (file.is_open()) {
+                planning_goal.path = filename;
+                planning_goal.reverse = true;  
+            }
+            else {
+                ROS_WARN("Unable to open waypoint file: %s", filename.c_str());
+                return false;
+            }
+        }
+        loadWaypoints(filename, waypoints_);
 
         planning_tracking_ac_.sendGoal(planning_goal,
                     boost::bind(&StateNode::PlanningActionDoneCb, this, boost::placeholders::_1, boost::placeholders::_2),
@@ -318,7 +335,9 @@ public:
                     boost::bind(&StateNode::PlanningActionFeedbackCb, this, boost::placeholders::_1));
 
         is_planning_action_finished_ = false;
-        is_reach_target_point = false;
+        is_reach_target_point_ = false;
+        progress_percentage_ = 0;
+
         ros::Rate loop_rate(0.5);
 
         while (!is_planning_action_finished_ && ros::ok()){
@@ -326,13 +345,119 @@ public:
             ros::spinOnce();
         }
 
-        if(is_reach_target_point)
-            return true; 
+        if(is_reach_target_point_)
+            return b; 
         else
-            return false;
+            return 0;
     }
 
-    bool collision_to_person(float range){
+    int request_planning_with_collision(int a, int b, bool indoor, float range){
+
+        std::ostringstream oss1;
+        std::ostringstream oss2;
+        std::string filename;
+        std::string out_or_in;
+        morai_woowa::Planning_Tracking_ActGoal planning_goal;
+
+        if (indoor) 
+            out_or_in = "indoor";
+        else
+            out_or_in = "outdoor";
+
+        oss1 << out_or_in << "_" << a << "_" << b << ".csv";
+        filename = oss1.str();
+
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            planning_goal.path = filename;  
+            planning_goal.reverse = false;  
+        }
+        else {
+            oss2 << out_or_in << "_" << b << "_" << a << ".csv";
+            filename = oss2.str();
+            std::ifstream file(filename);
+
+            if (file.is_open()) {
+                planning_goal.path = filename;
+                planning_goal.reverse = true;  
+            }
+            else {
+                ROS_WARN("Unable to open waypoint file: %s", filename.c_str());
+                return false;
+            }
+        }
+        loadWaypoints(filename, waypoints_);
+
+        //
+        planning_tracking_ac_.sendGoal(planning_goal,
+                    boost::bind(&StateNode::PlanningActionDoneCb, this, boost::placeholders::_1, boost::placeholders::_2),
+                    boost::bind(&StateNode::PlanningActionActiveCb, this),
+                    boost::bind(&StateNode::PlanningActionFeedbackCb, this, boost::placeholders::_1));
+
+        is_planning_action_finished_ = false;
+        is_reach_target_point_ = false;
+        progress_percentage_ = 0;
+
+        //
+        morai_woowa::Person_Collision_ActGoal collision_goal;
+        collision_goal.range = range; 
+
+        person_collision_ac_.sendGoal(collision_goal,
+                    boost::bind(&StateNode::PersonCollisionActionDoneCb, this, boost::placeholders::_1, boost::placeholders::_2),
+                    boost::bind(&StateNode::PersonCollisionActionActiveCb, this),
+                    boost::bind(&StateNode::PersonCollisionActionFeedbackCb, this, boost::placeholders::_1));
+
+
+        is_collision_action_finished_ = false;
+        is_success_collision_ = false;
+        is_found_collision_target_ = false;
+
+        bool tracking_stop_flag = false;
+
+        ros::Rate loop_rate(0.5);
+
+        while (!is_planning_action_finished_ && ros::ok()){
+            
+            if(is_found_collision_target_ && progress_percentage_ < 0.25 && !tracking_stop_flag){
+                
+                tracking_stop_flag = true; 
+
+                //충돌타겟 발견시 일단 트래킹 프로세스는 멈춰
+                morai_woowa::StopTrackingSrv stop_srv;
+                stop_srv.request.stop = true;
+                stop_tracking_client_.call(stop_srv);
+
+                //충돌에 집중
+                while (!is_collision_action_finished_ && ros::ok()){
+                    loop_rate.sleep();
+                    ros::spinOnce();
+                }
+                
+                // 충돌 성공시
+                if(is_success_collision_){
+                    planning_tracking_ac_.cancelGoal();
+                    return 0;
+                } 
+                // 충돌 실패시
+                else{
+                    stop_srv.request.stop = false;
+                    stop_tracking_client_.call(stop_srv);
+                }
+            }
+            
+            loop_rate.sleep();
+            ros::spinOnce();
+        }
+
+        person_collision_ac_.cancelGoal();
+
+        if(is_reach_target_point_)
+            return b; 
+        else
+            return a;
+    }
+
+    bool request_collision_to_person(float range){
 
         morai_woowa::Person_Collision_ActGoal collision_goal;
         collision_goal.range = range; 
@@ -343,7 +468,9 @@ public:
                     boost::bind(&StateNode::PersonCollisionActionFeedbackCb, this, boost::placeholders::_1));
 
         is_collision_action_finished_ = false;
-        is_success_collision = false;
+        is_success_collision_ = false;
+        is_found_collision_target_ = false;
+
         ros::Rate loop_rate(0.5);
 
         while (!is_collision_action_finished_ && ros::ok()){
@@ -351,23 +478,63 @@ public:
             ros::spinOnce();
         }
 
-        if(is_success_collision)
+        if(is_success_collision_)
             return true; 
         else
             return false;
     }
 
+    bool check_teleport_success(std::string type){
+
+        float dis = 10000;
+        
+        if (type == "teloport_indoor"){
+            Spot teloport_spot_indoor(3.00, -42.00);
+            dis = calculateDistance(current_x_, current_y_, teloport_spot_indoor.x, teloport_spot_indoor.y);
+        }
+        else if (type == "teloport_outdoor"){
+            Spot teloport_spot_outdoor(430.00, -140.00);
+            dis = calculateDistance(current_x_, current_y_, teloport_spot_outdoor.x, teloport_spot_outdoor.y);        
+        }
+        else if (type == "respawn_indoor"){
+            Spot respawn_spot_indoor(430.00, -140.00);
+            dis = calculateDistance(current_x_, current_y_, respawn_spot_indoor.x, respawn_spot_indoor.y);        
+        }
+        else if (type == "respawn_outdoor"){
+            Spot respawn_spot_outdoor(430.00, -140.00);
+            dis = calculateDistance(current_x_, current_y_, respawn_spot_outdoor.x, respawn_spot_outdoor.y);        
+        }
+        else{
+            ROS_WARN("teleport fail >> strange type : %s", type.c_str());
+            return false;
+        }    
+
+        if (dis < 1){
+            ROS_WARN("teleport success: %s", type.c_str());
+            return true;
+        }
+        else{
+            ROS_WARN("teleport fail: %s", type.c_str());
+            return false;
+        }
+    }
 
     // 여기가 메인 state 함수임!!
     void state() {
-        ros::Rate rate(100);  // 0.01 Hz
+        ros::Rate rate(20);  // 0.01 Hz
         while (ros::ok()) {
 
-            set_target_point(0,1,true);
+            request_planning_point(0,1,true);
 
-            collision_to_person(10);
+            request_collision_to_person(10);
+
+            check_teleport_success("respawn_indoor");
+
+            delivery(1);
+
+            pickup(1);
+
             PublishWaypoints(waypoints_);
-            publishMarker(waypoints_);
             
             rate.sleep();  // 지정된 주기로 대기
         }
@@ -380,22 +547,28 @@ private:
     actionlib::SimpleActionClient<morai_woowa::Person_Collision_ActAction> person_collision_ac_;
 
     ros::Subscriber current_pose_sub_;
-    ros::Publisher waypoint_pub_; // 
+    ros::Publisher waypoint_pub_; 
     ros::Publisher waypoint_marker_pub_;
+
     ros::ServiceClient delivery_pickup_client_;
+    ros::ServiceClient stop_tracking_client_;
 
     bool is_planning_action_finished_;
-    bool is_reach_target_point;
+    bool is_reach_target_point_;
+    float progress_percentage_;
 
+    bool is_found_collision_target_;
     bool is_collision_action_finished_;
-    bool is_success_collision;
+    bool is_success_collision_;
 
     path waypoints_;
 
-    int closest_index_ = 0;
-    double cross_track_error = 10000000; 
+    int closest_index_;
+    double cross_track_error; 
+    float current_x_;
+    float current_y_;
 
-    bool wpt_init_flag = false;
+    bool wpt_init_flag_;
     
 };
 
@@ -408,10 +581,11 @@ int main(int argc, char** argv) {
     StateNode.loadWaypoints("/path/to/waypoints.csv", waypoints);
     StateNode.PublishWaypoints(waypoints);
 
+    // std::thread thread(&StateNode::state, &StateNode);
 
     ros::spin();
     
-    //thread.join();  // 스레드가 종료될 때까지 대기
+    // thread.join();  
 
     return 0;
 }
