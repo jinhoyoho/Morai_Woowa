832172727f380a66012bce59a92e73e5f00ea804
freshleesh <freshleesh@gmail.com>
2024-10-24 17:15:44 +0900

    Fix: state 주석 추가

--- src/state_node copy 2.cpp
+++ src/state_node copy 2.cpp
@@ -44,14 +44,15 @@ public:
     StateNode(): 
     nh_("~"),
     planning_tracking_ac_(nh_, "/planning_tracking", true),
+    person_collision_ac_(nh_, "/person_collision_action", true),
     person_collision2_ac_(nh_, "/person_collision_action2", true)
     {
         dilly_item_status_sub_ = nh_.subscribe("/WoowaDillyStatus", 10, &StateNode::itemstatusCallback, this);
         current_pose_sub_ = nh_.subscribe("/current_pose", 10, &StateNode::currentPoseCallback, this);
         waypoint_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/waypoints", 10);  
 
-        delivery_pickup_client_ = nh_.serviceClient<morai_msgs::WoowaDillyEventCmdSrv>("/WoowaDillyEventCmd"); // 배달, 픽업 용 서비스, 모라이 서버와 통신
-        control_mode_client_ = nh_.serviceClient<morai_woowa::ControlSrv>("/Control_srv"); // 컨트롤 노드
+        delivery_pickup_client_ = nh_.serviceClient<morai_msgs::WoowaDillyEventCmdSrv>("/WoowaDillyEventCmd");
+        control_mode_client_ = nh_.serviceClient<morai_woowa::ControlSrv>("/Control_srv");
 
         if (planning_tracking_ac_.waitForServer(ros::Duration(5.0))) {  // 5초 대기
             ROS_INFO("Connected to planning_tracking server");
@@ -75,6 +76,8 @@ public:
 
         wpt_init_flag_ = false;
 
+        closest_index_ = -1;
+        cross_track_error = 10000000; 
         current_x_ = 0;
         current_y_ = 0;
 
@@ -95,27 +98,70 @@ public:
     }
 
     void currentPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-        // 현재 xy 좌표 (dilly)
+        
         current_x_ = msg->pose.position.x;
         current_y_ = msg->pose.position.y;
+
+        closest_index_ = findClosestWaypoint(current_x_, current_y_, closest_index_, waypoints_);
+        
+        // if (closest_index_ != -1) {
+        //     ROS_INFO("Current Position: (X: %.2f, Y: %.2f) is nearest to Waypoint Index: %d", current_x_, current_x_, closest_index_);
+        //     ROS_INFO("cross_track_error: %lf", cross_track_error);
+        // } else {
+        //     ROS_WARN("No waypoints available.");
+        // }
     }
 
+    // 현재 경로 pub
+    void PublishWaypoints(path waypoints_) {
+        for (const auto& waypoint : waypoints_) {
+            geometry_msgs::PoseStamped pose_stamped;
+            pose_stamped.header.frame_id = "map";
+            pose_stamped.header.stamp = ros::Time::now();
+
+            // Waypoint 좌표 설정
+            pose_stamped.pose.position.x = waypoint.x;
+            pose_stamped.pose.position.y = waypoint.y;
+            pose_stamped.pose.position.z = 0;
+
+            // Heading 값을 그대로 사용하여 orientation 설정
+            tf::Quaternion q = tf::createQuaternionFromYaw(waypoint.heading); // 여전히 쿼터니언 생성이 필요할 경우
+            pose_stamped.pose.orientation.x = q.x();
+            pose_stamped.pose.orientation.y = q.y();
+            pose_stamped.pose.orientation.z = q.z();
+            pose_stamped.pose.orientation.w = q.w();
+
+            // PoseStamped 퍼블리시
+            waypoint_pub_.publish(pose_stamped);
+        }
+    }
 
     // 배달 함수
     bool delivery(int item_index){
-
-        morai_msgs::WoowaDillyEventCmdSrv deli_srv;
-        deli_srv.request.request.isPickup = true; // 배달 준비완료
-        deli_srv.request.request.deliveryItemIndex = item_index; // 배달할 아이템 번호
         
-        delivery_pickup_client_.call(deli_srv);  // 서비스 요청
+        morai_msgs::WoowaDillyEventCmdSrv deli_srv;
+        deli_srv.request.request.isPickup = true; 
+        deli_srv.request.request.deliveryItemIndex = item_index; 
+        
+        delivery_pickup_client_.call(deli_srv);
+        
+        // ros::Rate loop_rate(0.5);
+        // int cnt = 0;
 
-        return deli_srv.response.response.result; // 결과
+        // while (!deli_srv.response.response.result && ros::ok() && cnt < 5){
+        //     std::cout << "state node : Failed to delivery. Retrying..." << std::endl;
+        //     delivery_pickup_client_.call(deli_srv);
+        //     loop_rate.sleep();
+        //     ros::spinOnce();
+        //     cnt ++;
+        // }
+
+        return deli_srv.response.response.result;
     } 
 
     // 픽업 함수
     bool pickup(int item_index){
-        // 이미 가지고 있는지 확인
+
         for(auto i : dilly_item_status_list_){
             if(i == item_index){
                 ROS_INFO("I already have it");
@@ -124,39 +170,121 @@ public:
         }
 
         morai_msgs::WoowaDillyEventCmdSrv pick_srv;
-        pick_srv.request.request.deliveryItemIndex = item_index; // 아이템 번호
-        pick_srv.request.request.isPickup = true; // 준비 완료
+        pick_srv.request.request.deliveryItemIndex = item_index;
+        pick_srv.request.request.isPickup = true;
 
-        delivery_pickup_client_.call(pick_srv); // 요청
+        delivery_pickup_client_.call(pick_srv);
 
-        bool result = pick_srv.response.response.result; // 결과
+        bool result;
 
         if(!result)
             ROS_WARN("PICKUP FAIL");
 
         return result;
     }
-    
-    // 컨트롤 노드에 모드를 변경하는 함수
+
     void change_control_mode(int mode){
         morai_woowa::ControlSrv control_srv;
         control_srv.request.mode = mode;
         control_mode_client_.call(control_srv);
     }
 
+    // 파일이름으로 Waypoints load
+    void loadWaypoints(std::string waypoint_file, path &waypoints) {
+
+        wpt_init_flag_ = false;
+        int cnt = 0;
+
+        std::ifstream file(waypoint_file);
+        if (file.is_open()) {
+            Waypoint wp;
+            std::string line;
+
+            // CSV 파일의 각 줄을 읽음
+            while (std::getline(file, line)) {
+
+                if (cnt == 0) {
+                    // 첫 줄은 데이터 프레임
+                    cnt ++;
+                    continue;
+                }
+
+                std::istringstream ss(line);
+                std::string token;
+
+                std::getline(ss, token, ','); // x
+                wp.x = std::stod(token);
+                std::getline(ss, token, ','); // y
+                wp.y = std::stod(token);
+                std::getline(ss, token, ','); // heading
+                wp.heading = std::stod(token);
+                std::getline(ss, token, ','); // index
+                wp.index = std::stoi(token);
+                
+                waypoints_.push_back(wp);
+
+                cnt ++;
+            }
+            file.close();
+            ROS_INFO("Loaded waypoints from %s", waypoint_file.c_str());
+        } else {
+            ROS_WARN("Unable to open waypoint file: %s", waypoint_file.c_str());
+        }
+    }
+
+    // 현재위치에서 가장 가까운 경로 idx 
+    int findClosestWaypoint(float x, float y, int previous_index, path waypoints) {
+
+        // 처음엔 모든 wpt를 돌면서 위치 체크
+        if(!wpt_init_flag_){
+            int closest_index = -1;
+            double min_distance = std::numeric_limits<double>::max();
+
+            for (int i = 0; i < waypoints.size(); i++) {
+                const auto& wp = waypoints[i];
+                double distance = calculateDistance(x, y, wp.x, wp.y);
+                if (distance < min_distance) {
+                    min_distance = distance;
+                    closest_index = wp.index;
+                    cross_track_error = min_distance;
+                }
+            }
+            wpt_init_flag_ = true;
+
+            return closest_index;
+        }
+
+        // 이후부터는 연산량을 위해 이전 wpt 앞뒤로 5개 점만 보고 가까운 idx찾기
+        int start_index = std::max(previous_index - 5, 0);
+        int end_index = std::min(previous_index + 5, static_cast<int>(waypoints.size()) - 1);
+
+        int closest_index = -1;
+        double min_distance = std::numeric_limits<double>::max();
+
+        for (int i = start_index; i <= end_index; ++i) {
+
+            const auto& wp = waypoints[i];
+            double distance = calculateDistance(x, y, wp.x, wp.y);
+            if (distance < min_distance) {
+                min_distance = distance;
+                closest_index = wp.index;
+                cross_track_error = min_distance;
+            }
+        }
+
+        return closest_index;
+    }
 
     double calculateDistance(double x1, double y1, double x2, double y2) {
         return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
     }
 
-    // is_reach_target_point_를 변경하기 위한 함수
     void PlanningActionDoneCb(const actionlib::SimpleClientGoalState& state,
                               const morai_woowa::Planning_Tracking_ActResultConstPtr& result){
-        // 플래닝 액션이 완료됐는지
+        
         is_planning_action_finished_ = true;
 
         if (result->success){
-            // 플래닝 액션 성공하면 true로
             is_reach_target_point_ = true;
             std::cout << "state node : planning action finished successfully" << std::endl;
         } 
@@ -175,7 +303,6 @@ public:
         progress_percentage_ = feedback->progress_percentage;
     }
 
-    // is_success_collision_ 변경하기 위한 함수
     void PersonCollisionActionDoneCb(const actionlib::SimpleClientGoalState& state,
                               const morai_woowa::Person_Collision_Act2ResultConstPtr& result){
         
@@ -205,14 +332,13 @@ public:
     // 도착 성공시 arrival_point 반환
     int request_planning(int starting_point, int arrival_point, bool indoor){
         
-        change_control_mode(1);  // planning mode 로 실행
+        change_control_mode(1);
 
         std::ostringstream oss;
         std::ostringstream oss2;
         std::string filename;
         std::string full_filename;
         std::string out_or_in;
-
         morai_woowa::Planning_Tracking_ActGoal planning_goal;
 
         if (indoor) 
@@ -243,6 +369,7 @@ public:
                 planning_goal.path = filename;
                 planning_goal.reverse = true;  
                 ROS_INFO("Success open wpt file: %s", filename.c_str());
+
             }
             // 이래도 파일 안열리면 걍 없는 경로임 오류 생성
             else {
@@ -251,6 +378,8 @@ public:
             }
         }
 
+        loadWaypoints(full_filename, waypoints_);
+
         // 경로 트래킹 요청
         planning_tracking_ac_.sendGoal(planning_goal,
                     boost::bind(&StateNode::PlanningActionDoneCb, this, boost::placeholders::_1, boost::placeholders::_2),
@@ -258,28 +387,25 @@ public:
                     boost::bind(&StateNode::PlanningActionFeedbackCb, this, boost::placeholders::_1));
 
         // 관련 멤버변수 초기화
-        is_planning_action_finished_ = false;   // 플래닝 액션이 완료됐는지
+        is_planning_action_finished_ = false;
         is_reach_target_point_ = false;
         progress_percentage_ = 0;
 
         ros::Rate loop_rate(0.5);
 
         while (!is_planning_action_finished_ && ros::ok()){
-            // 각종 플래그들 갱신
             loop_rate.sleep();
             ros::spinOnce();
         }
-        
-        // target point에 도달했는지 -> DoneCb에서 갱신
+
         if(is_reach_target_point_)
             return arrival_point; 
         else
             return 0;
     }
 
-    // 몇번(spot) 사람을 부딪히게 할 건지
     bool request_collision_to_person(int spot, bool in_indoor){
-        // collision mode로 변경
+
         change_control_mode(2);
 
         morai_woowa::Person_Collision_Act2Goal collision_goal;
@@ -309,7 +435,6 @@ public:
             return false;
     }
 
-    // teleport가 성공했는지
     bool check_teleport_success(std::string type){
 
         float dis = 10000;
@@ -345,13 +470,11 @@ public:
         }
     }
 
-    // 도착했는지 여부 확인
     bool check_arrival_success(int point, bool indoor){
 
         float dis = 10000;
         Spot target_spot(-99999,-99999);
-
-        // 실내일때
+        
         if(indoor)
         { // 실내
             if(point == 1){    
@@ -379,7 +502,7 @@ public:
                 return false;
             }  
         }
-        else    // 야외일때
+        else
         {
             if(point == 1){    
                 target_spot.x = 393.9;
@@ -406,7 +529,7 @@ public:
                 return false;
             }    
         }
-        // 거리 계산
+        
         dis = calculateDistance(current_x_, current_y_, target_spot.x, target_spot.y);        
 
         if (dis < 1){
@@ -424,26 +547,25 @@ public:
         ros::Rate rate(20);  // 0.01 Hz
         while (ros::ok()) {
 
-            int starting_point; // 시작점
-            int arrival_point;  // 도착점
-            bool is_indoor;     // 실내인지
-            int arrival_result; // 도착결과
+            int starting_point;
+            int arrival_point;
+            bool is_indoor;
+            int arrival_result;
 
-            int item_index;     // 아이템 번호
-            bool delivery_result;   // 배달 결과
+            int item_index;
+            bool delivery_result;
 
             // starting_point에서 arrival_point로 가라(실내/실외)
             // return: (int) arrival point (1번~5번) 지점
             // 실패하면 0 반환
-            starting_point = 0;
-            arrival_point = 4;
-            is_indoor = true;
-            arrival_result = 0;
-            // 도착할때까지 while문 반복
-            while(ros::ok() && arrival_result != arrival_point){
-                arrival_result = request_planning(starting_point, arrival_point, is_indoor);    
-                std::cout << "arrival_result " << arrival_result << std::endl;
-            }
+            // starting_point = 0;
+            // arrival_point = 4;
+            // is_indoor = true;
+            // arrival_result = 0;
+            // while(ros::ok() && arrival_result != arrival_point){
+            //     arrival_result = request_planning(starting_point, arrival_point, is_indoor);    
+            //     std::cout << "arrival_result " << arrival_result << std::endl;
+            // }
             
             // // pickup
             // item_index = 4;
@@ -452,7 +574,7 @@ public:
             //     delivery_result = pickup(item_index);    
             // }
 
-            // request_collision_to_person(5, true);
+            request_collision_to_person(5, true);
 
             // starting_point = 0;
             // arrival_point = 5;
@@ -473,7 +595,7 @@ public:
             // }
 
             // 사람에 부딪혀라 -> 안 쓸것같음
-            // request_collision_to_person(5, true);
+            request_collision_to_person(5, true);
 
             // int starting_point2 = 0;
             // int arrivel_point2 = 1;
@@ -497,6 +619,7 @@ public:
             // delivery(item_index1);
 
             // // request planning 문자열 조합, state에서 publish, 확인용
+            // PublishWaypoints(waypoints_);
             
             rate.sleep();  // 지정된 주기로 대기
         }
@@ -506,6 +629,7 @@ private:
     ros::NodeHandle nh_;
 
     actionlib::SimpleActionClient<morai_woowa::Planning_Tracking_ActAction> planning_tracking_ac_;
+    actionlib::SimpleActionClient<morai_woowa::Person_Collision_Act2Action> person_collision_ac_;
     actionlib::SimpleActionClient<morai_woowa::Person_Collision_Act2Action> person_collision2_ac_;
 
     ros::Subscriber current_pose_sub_;
@@ -523,7 +647,10 @@ private:
     bool is_collision_action_finished_;
     bool is_success_collision_;
 
+    path waypoints_;
 
+    int closest_index_;
+    double cross_track_error; 
     float current_x_;
     float current_y_;
 
